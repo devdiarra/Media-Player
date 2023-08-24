089a3e8c62974593a222d77e032dfb1d54d62667
Diarra Moustapha <diarra.msa.pro@gmail.com>
2023-08-24 21:11:13 +0300

    -Add methods to model.py -Write the code for Player class. This class is responsible for the handling of audio playback and -Modifing the view.py by adding news methods.

--- player.py
+++ player.py
@@ -1,3 +1,87 @@
+import pyglet.media
+
+FORWARD_REWIND_JUMP_TIME = 20
+
+
 class Player:
     def __init__(self):
-        pass
+        self.source = None
+        self.player = pyglet.media.Player()
+        self.player.volume = 0.6
+
+    def play_media(self, audio_file):
+        self.reset_play()
+        self.player = pyglet.media.Player()
+        self.source = pyglet.media.load(audio_file)
+        self.player.queue(self.source)
+        self.player.play()
+
+    def reset_play(self):
+        self.player.pause()
+        self.player.delete()
+
+    @property
+    def is_playing(self):
+        try:
+            elapsed_time = int(self.player.time)
+            is_playing = elapsed_time < int(self.track_length)
+        except:
+            is_playing = False
+        return is_playing
+
+    def seek(self, time):
+        try:
+            self.player.seek(time)
+        except AttributeError:
+            pass
+
+    @property
+    def track_length(self):
+        try:
+            return self.source.duration
+        except AttributeError:
+            return 0
+
+    @property
+    def volume(self):
+        return self.player.volume
+
+    @property
+    def elapsed_play_duration(self):
+        return self.player.time
+
+    @volume.setter
+    def volume(self, volume):
+        self.player.volume = volume
+
+    def unpause(self):
+        self.player.play()
+
+    def pause(self):
+        self.player.pause()
+
+    def stop(self):
+        self.reset_play()
+
+    def mute(self):
+        self.player.volume = 0.0
+
+    def unmute(self, newvolume_level):
+        self.player.volume = newvolume_level
+
+    def fast_forward(self):
+        time = self.player.time + FORWARD_REWIND_JUMP_TIME
+        try:
+            if self.source.duration > time:
+                self.seek(time)
+            else:
+                self.seek(self.source.duration)
+        except AttributeError:
+            pass
+
+    def rewind(self):
+        time = self.source.duration - FORWARD_REWIND_JUMP_TIME
+        try:
+            self.seek(time)
+        except:
+            self.seek(0)
--- view.py
+++ view.py
@@ -1,3 +1,4 @@
+import os.path
 import tkinter as tk
 import tkinter.filedialog
 import tkinter.messagebox
@@ -18,7 +19,7 @@ class View:
         self.create_top_display()
         self.create_button_frame()
         self.create_list_box()
-        self.create_button_frame()
+        self.create_bottom_frame()
         self.create_context_menu()
 
     def create_top_display(self):
@@ -30,8 +31,78 @@ class View:
     def create_list_box(self):
         pass
 
+    def create_bottom_frame(self):
+        pass
+
     def create_context_menu(self):
         pass
+
+    def on_previous_track_button_clicked(self):
+        pass
+
+    def on_rewind_button_clicked(self):
+        pass
+
+    def on_play_stop_button_clicked(self):
+        pass
+
+    def on_pause_unpause_button_clicked(self):
+        pass
+
+    def on_mute_unmute_button_clicked(self):
+        pass
+
+    def on_fast_forward_button_clicked(self):
+        pass
+
+    def on_next_track_button_clicked(self):
+        pass
+
+    def on_volume_scale_changed(self, value):
+        pass
+
+    def on_add_file_button_clicked(self):
+        self.add_audio_file()
+
+    def on_remove_selected_button_clicked(self):
+        self.remove_selected_files()
+
+    def on_add_directory_button_clicked(self):
+        self.add_all_audio_files_from_directory()
+
+    def on_empty_play_list_button_clicked(self):
+        pass
+
+    def on_clear_play_list_button_clicked(self):
+        self.clear_play_list()
+
+    def on_remove_selected_context_menu_clicked(self):
+        self.remove_selected_files()
+
+    def on_play_list_double_clicked(self, event=None):
+        pass
+
+    def add_audio_file(self):
+        audio_file = tkinter.filedialog.askopenfilename(
+            filetypes=[
+                ("All supported", ".mp3 .waw"),
+                (".mp3 files", ".mp3"), (".waw files", ".waw"),
+            ]
+        )
+        if audio_file:
+            self.model.add_to_play_list(audio_file)
+            file_path, file_name = os.path.split(audio_file)
+            self.list_box.insert(tk.END, file_name)
+
+    def remove_selected_files(self):
+        try:
+            selected_indexes = self.list_box.curselection()
+            for index in reversed(selected_indexes):
+                self.lis_box.delete(index)
+                self.model.remove_item_from_play_list_at_index(index)
+        except IndexError:
+            pass
+
 
 
 if __name__ == "__main__":
