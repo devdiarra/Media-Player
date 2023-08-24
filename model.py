bf676211b82d9de93a3e6582028c3eeaeb30d9dc
Diarra Moustapha <diarra.msa.pro@gmail.com>
2023-08-25 02:30:47 +0300

    continue coding by adding some methods

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ class Model:
     def get_file_to_play(self, file_index):
         return self.__play_list[file_index]
 
-    def clear_play_list(self):
+    def empty_play_list(self):
         self.__play_list.clear()
 
     def add_to_play_list(self, file_name):
--- view.py
+++ view.py
@@ -1,13 +1,21 @@
-import os.path
+import os
 import tkinter as tk
 import tkinter.filedialog
 import tkinter.messagebox
 import tkinter.ttk as ttk
-
+import itertools
 import model
 import player
 
+
+
 class View:
+    current_track_index = 0
+    toggle_play_stop = itertools.cycle(["play", "stop"])
+    toggle_pause_unpause = itertools.cycle(["pause", "unpause"])
+    toggle_mute_unmute = itertools.cycle(["mute", "unmute"
+                                          ])
+
     def __init__(self, root, model, player):
         self.root = root
         self.model = model
@@ -38,28 +46,56 @@ class View:
         pass
 
     def on_previous_track_button_clicked(self):
-        pass
+        self.play_previous_track()
 
     def on_rewind_button_clicked(self):
-        pass
+        self.player.rewind()
 
     def on_play_stop_button_clicked(self):
-        pass
+        action = next(self.toggle_play_stop)
+        if action == "play":
+            try:
+                self.current_track_index = self.list_box.curselection()[0]
+            except IndexError:
+                self.current_track_index = 0
+            self.start_play()
+        elif action == 'stop':
+            self.stop_play()
+
+
 
     def on_pause_unpause_button_clicked(self):
-        pass
+        action = next(self.toggle_pause_unpause)
+        if action == 'pause':
+            self.player.pause()
+        elif action == 'unpause':
+            self.player.unpause()
 
     def on_mute_unmute_button_clicked(self):
-        pass
+        action = next(self.toggle_mute_unmute)
+        if action == 'mute':
+            self.volume_at_time_of_mute = self.player.volume
+            self.player.mute()
+            self.volume_scale.set(0)
+            self.mute_unmute_button.config(imgage=self.mute_icon)
+        elif action == 'unmute':
+            self.player.unmute(self.volume_at_time_of_mute)
+            self.volume_scale.set(self.volume_at_time_of_mute)
+            self.mute_unmute_button.config(images=self.unmute_icon)
+
 
     def on_fast_forward_button_clicked(self):
-        pass
+        self.player.fast_forward()
 
     def on_next_track_button_clicked(self):
-        pass
+        self.play_next_track()
 
     def on_volume_scale_changed(self, value):
-        pass
+        self.player.volume = self.volume_set.get()
+        if self.volume_scale.get() == 0.0:
+            self.mute_unmute_button.config(image=self.mute_icon)
+        else:
+            self.on_mute_unmute_button.config(image=self.unmute_icon)
 
     def on_add_file_button_clicked(self):
         self.add_audio_file()
@@ -80,7 +116,8 @@ class View:
         self.remove_selected_files()
 
     def on_play_list_double_clicked(self, event=None):
-        pass
+        self.current_track_index = int(self.list_box.curselection()[0])
+        self.start_play()
 
     def add_audio_file(self):
         audio_file = tkinter.filedialog.askopenfilename(
@@ -103,6 +140,49 @@ class View:
         except IndexError:
             pass
 
+    def add_all_audio_files_from_directory(self):
+        directory_path = tkinter.filedialog.askdirectory()
+        if not directory_path: return
+        audio_files_in_directory = self.get_all_audio_file_from_directory(directory_path)
+        for audio_file in audio_files_in_directory:
+            self.model.add_to_play_list(audio_file)
+            file_path, file_name = os.path.split(audio_file)
+            self.list_box.insert(tk.END, file_name)
+
+    def get_all_audio_file_from_directory(self, directory_path):
+        audio_files_in_directory = []
+        for (dirpath, dirnames, filenames) in os.walk(directory_path):
+            for audio_file in filenames:
+                if audio_file.endswith(".mp3") or audio_file.endswith(".waw"):
+                    audio_files_in_directory.append(dirpath + "/" +  audio_file)
+        return audio_files_in_directory
+
+    def empty_play_list(self):
+        self.model.empty_list()
+        self.list_box.delete(0, tk.END)
+
+    def start_play(self):
+        try:
+            audio_file = self.model.get_file_to_play(self.current_track_index)
+        except IndexError:
+            return
+        self.on_play_stop_button.config(image=self.stop_icon)
+        self.player.play_media(audio_file)
+
+    def stop_play(self):
+        self.play_stop_button.config(images=self.play_icon)
+        self.player.stop()
+
+    def clear_play_list(self):
+        pass
+
+    def play_previous_track(self):
+        self.current_track_index = max(0, self.current_track_index - 1)
+        self.start_play()
+
+    def play_next_track(self):
+        self.current_track_index = min(self.list_box.size() - 1, self.current_track_index + 1)
+        self.start_play()
 
 
 if __name__ == "__main__":
